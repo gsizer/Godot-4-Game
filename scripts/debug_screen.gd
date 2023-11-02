fdbd9e81e554dd08ed3ca6aaa1f6caf54d353c7a
gsizer <74977308+gsizer@users.noreply.github.com>
2023-11-02 10:19:25 -0700

    Update debug_screen.gd

--- scripts/debug_screen.gd
+++ scripts/debug_screen.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var CTRL : Controller
+
 @onready var label : Label = $MarginContainer/Label
 
 var framerate : float = 0.0
@@ -7,7 +9,7 @@ var framecount : int = 0
 var lastTime : float = 0.0
 var currentTime : float = 0.0
 var debugInfo : String = ""
-var debugString : String = "Frames Per Second: {FPS}\n"
+var debugString : String = "FPS: {FPS}"
 
 func _input(event):
 	if event.is_action_released("toggle_debug_info"):
@@ -28,6 +30,7 @@ func _process(_delta):
 		lastTime = currentTime
 		# update Label Text if visible
 		if is_visible_in_tree():
+			debugInfo = ""
 			# apply variables to debugString
 			debugInfo = debugString.format( {"FPS":framerate} )
 			label.set("text", debugInfo)
