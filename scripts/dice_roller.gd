40b7d67705c4aa6b222ed01a1d0bf12886c2f923
gsizer <74977308+gsizer@users.noreply.github.com>
2023-11-03 21:58:53 -0700

    Update dice_roller.gd

--- scripts/dice_roller.gd
+++ scripts/dice_roller.gd
@@ -1,20 +1,31 @@
 extends Node
 
-enum { D4=4, D6=6, D8=8, D10=10, D20=20, D100=100 }
+enum Dice { D4=4, D6=6, D8=8, D10=10, D20=20, D100=100 }
+
+var Count : int = 0
+var Die : Dice = Dice.D4
+var Rolls : Array = []
+var Total : int = 0
+var RollResult : Dictionary = { "Count"=0, "Faces"=Dice.D4, "Results"=Rolls, "Total"=0 }
+
 @export var RollHistory : Dictionary = {}
 
 ################################################################################
 # Accepts COUNT:int of dice with FACES:int
 # Returns Array of Int
-func Roll( Count:int=1, Faces:=D4, StoreResults:bool=false ) -> Array:
-	var rolls : Array = []
-	for c in Count:
-		rolls.append( randi_range(1,Faces) )
-	if StoreResults:
-		print_debug("Roll: {C}D{F}\nResult: {R}".format({"C":Count, "F":Faces, "R":rolls}))
-		RollHistory[RollHistory.size()] = rolls
-	return rolls
+func Roll( count:int=1, faces:Dice=Dice.D4 ) -> Array:
+	Rolls.clear()
+	var thisResult = RollResult
+	for c in count:
+		Rolls.append( randi_range(1, faces) )
+	thisResult["Count"]=count
+	thisResult["Faces"]=faces
+	thisResult["Rolls"]=Rolls
+	thisResult["Total"]=Total
+	RollHistory[RollHistory.size()] = thisResult
+	return Rolls
 
 func _ready():
+	Rolls.clear()
 	RollHistory.clear()
-	Roll( 10, D4, true )
+	Roll()
