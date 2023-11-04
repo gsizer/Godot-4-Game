8a74de2cee945eff71fa61453ae8b922d2c55179
gsizer <74977308+gsizer@users.noreply.github.com>
2023-11-04 00:24:17 -0700

    restart terrain

--- scripts/new_game.gd
+++ scripts/new_game.gd
@@ -1,19 +1,8 @@
 extends Node3D
 
 @export var PlayerCharacter : CharacterBody3D
-@export var WorldMap : String = "res://scenes/world_terrain_3d.tscn"
 var CTRL : Controller
-var WorldData : Node
 
 func _ready():
 	CTRL = find_parent("GameController") as Controller
-	WorldData = Adopt(WorldMap)
 	CTRL.StartGame()
-
-# become parent of Node
-func Adopt( scene ) -> Node:
-	var resNode = load(scene)
-	var instNode = resNode.instantiate()
-	add_child(instNode)
-	instNode.reparent(self)
-	return instNode
