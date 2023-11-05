94df959c005ec6158355ee3dab9c114983e81792
gsizer <74977308+gsizer@users.noreply.github.com>
2023-11-05 08:52:45 -0800

    quest & player update

--- scripts/quest.gd
+++ scripts/quest.gd
@@ -1,27 +1,37 @@
 class Quest extends Node:
 	
 	enum Category { Visit=10, Kill=20, Defend=30, Escort=40 }
-	
+	enum Status { Locked=0, Available=10, InProgress=20, Complete=30, Rewarded=40, Failed=50, Aborted=60}
 	var Objective : Dictionary = {
 		"Category":Category.Visit,
 		"Location":Vector3.ZERO,
 		"Destination":Vector3.ONE,
-		"Target":"",
-		"Count":0
+		"Target":Node,
+		"Status":Status.Locked
 	}
 	
-	var Name : String = ""
+	var Title : String = ""
 	var Description : String = ""
 	var Objectives : Array = []
 	
 	func _init():
+		Objective.make_read_only()
 		Objectives.clear()
 	
 	func AddObjective(objective:Dictionary)->void:
 		Objectives.append(objective)
+		print_debug(
+			"Quest: {T}\nAdded Objective:{I}:{O}".format(
+				{"T":Title, "I":Objectives.size(), "O":objective}))
+	
+	func NewObjective() -> Dictionary:
+		var newObj = Objective
+		return newObj
 	
 	func RemoveObjective(objective:Dictionary)->void:
 		if Objectives.has(objective):
 			var idx:int = Objectives.find(objective)
-			Objectives.pop_at( idx )
-	
+			var obj = Objectives.pop_at( idx )
+			print_debug(
+				"Quest: {T}\nRemoved Objective:{I}:{O}".format(
+					{"T":Title, "I":idx, "O":obj}))
